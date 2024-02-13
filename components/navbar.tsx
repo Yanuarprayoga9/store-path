6dbb828e0e83d016ec71b78ab6974045e6392a3e
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-13 19:03:58 +0700

    feat: mainpage

---
+++ components/navbar.tsx
@@ -0,0 +1,18 @@
+
+"use client"
+import React from "react";
+import Container from "./ui/container";
+import Btn from "./ui/btn-loader";
+
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <Container>
+        Navbar!
+       
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
