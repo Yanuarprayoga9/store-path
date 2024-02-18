43fb4c2e5358a1a9652cc3c5b0fc3a8903b4e96f
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-18 14:18:32 +0700

    feat: summary order

---
+++ app/(routes)/cart/components/summary.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+const Summary = () => {
+  return (
+    <div
+      className="
+    mt-16 rounded-lg bg-gray px-4 py-6 xm:p-6  lg:col-span-5 lg:mt-0 lg:p-8"
+    >
+      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+    </div>
+  );
+};
+
+export default Summary;
--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import Container from '@/components/ui/container';
 import useCart from '@/hooks/use-cart';
 
-// import Summary from './components/summary'
+import Summary from './components/summary'
 import CartItem from './components/cart-item';
 
 export const revalidate = 0;
@@ -28,7 +28,7 @@ const CartPage = () => {
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
-            <div className="lg:col-span-10">
+            <div className="lg:col-span-7">
               {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
               <ul>
                 {cart.items.map((item) => (
@@ -36,7 +36,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
-            {/* <Summary /> */}
+            <Summary />
           </div>
         </div>
       </Container>
