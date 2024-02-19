b2d72180ed24c91ce14f67cb76e1ef49329570a5
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-19 11:14:33 +0700

    feat: stuff

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,14 +1,58 @@
 "use client";
 
-import React from "react";
-
+import Button from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
 const Summary = () => {
+  const searchParams = useSearchParams();
+  const items = useCart((state) => state.items);
+  const removeAll = useCart((state) => state.removeAll);
+
+  useEffect(() => {
+    if (searchParams.get("success")) {
+      toast.success("Payment completed.");
+      removeAll();
+    }
+
+    if (searchParams.get("canceled")) {
+      toast.error("Something went wrong.");
+    }
+  }, [searchParams, removeAll]);
+
+  const totalPrice = items.reduce((total, item) => {
+    return total + Number(item.price);
+  }, 0);
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `http://localhost:3000/api/a3486c42-a607-43fe-b75a-3ad74767d41a/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+
+    window.location = response.data.url;
+  };
   return (
     <div
       className="
-    mt-16 rounded-lg bg-gray px-4 py-6 xm:p-6  lg:col-span-5 lg:mt-0 lg:p-8"
+    mt-16  bg-gray-50 px-4 py-6 xm:p-6  lg:col-span-5 lg:mt-0 lg:p-8 rounded-xl"
     >
       <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+      <div className="mt-6 space-y-4">
+        <div className="flex items-center flex-between border-t border-gray-200 pt-4">
+          <div className="text-base font-medium text-gray-900">Order Total <Currency value={ totalPrice} /></div>
+          
+        </div>
+      </div>
+      <Button 
+      onClick={onCheckout}
+      disabled={items.length === 0}
+      className=" w-full mt-6"
+      >Checkout</Button>
     </div>
   );
 };
